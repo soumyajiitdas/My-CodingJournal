2a501189681db66ae7c5ce369bd8ef63b0907412
Soumyajit Das <116360739+Soumyajit4119@users.noreply.github.com>
2025-05-03 23:12:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (60.69%) - LeetSync

---
+++ LeetCode-Solutions/50-powx-n/powx-n.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        half = self.myPow(x, n//2)
+
+        if n % 2 == 0:
+            return half * half
+        else:
+            return x * half * half
+
+
+        
